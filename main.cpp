493410b2e29dad8f10910a830b81f6dd2ec0f1c9
ama3489 <32362357+ama3489@users.noreply.github.com>
2018-10-15 10:55:32 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+//void printBoard(const string board[][COLUMN_SIZE])
+//{
+    //if()
+//}
+
+void mainMenu()
+{
+    cout <<"Main Menu" << "\n";
+}
+
+int main()
+{
+    string boardsize;
+
+    mainMenu();
+    cout <<"Welcome to CONNECT FOUR.";
+}
